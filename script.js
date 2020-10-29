c8611893787f763e62fda6206b8ad02b507832f2
epeppin <epeppin@my.wctc.edu>
2020-10-28 20:57:07 -0500

    Convert to jQuery 2

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-$(document).ready (
+$(document).ready(
+    function () {
+        // Store Late fee's
+        var lateBook = 0.25
+        var lateDVD = 0.50
 
-function() {
-    // Store Late fee's
-    var lateBook = 0.25
-    var lateDVD = 0.50
+        //add event handler
 
-    //add event handler
+        $("#calcButton").click(calcLateFee);
 
-    $("#calcButton").click(calcLateFee);
+        function calcLateFee() {
+            // get data from late books input and parse to number
+            var numBooks = parseInt($("#numBooks").val());
 
-    function calcLateFee() {
-        // get data from late books input and parse to number
-        var numBooks = parseInt($("#numBooks").val());
+            //Collect Number of late DVD's input and parse to number
+            var numDvds = parseInt($("#numDvds").val());
 
-        //Collect Number of late DVD's input and parse to number
-        var numDvds = parseInt($("#numDvds").val());
+            //Collect number of days late and parse to number
+            var numDaysLate = parseInt($("#numDays").val());
 
-        //Collect number of days late and parse to number
-        var numDaysLate = parseInt($("#numDays").val());
+            // Convert to a number data type (no decimals)
+            numDaysLate = parseInt(numDaysLate)
 
-        // Convert to a number data type (no decimals)
-        numDaysLate = parseInt(numDaysLate)
+            // multiply number of late books by days late then by late fee
+            var calcLateBooks = (numBooks * numDaysLate) * lateBook;
 
-        // multiply number of late books by days late then by late fee
-        var calcLateBooks = (numBooks * numDaysLate) * lateBook;
+            //Multiply number of late DVD's by days late then by late fee
+            var calcLateDvds = (numDvds * numDaysLate) * lateDVD;
 
-        //Multiply number of late DVD's by days late then by late fee
-        var calcLateDvds = (numDvds * numDaysLate) * lateDVD;
+            //Add Totals Together
+            var totalLateFee = calcLateBooks + calcLateDvds
 
-        //Add Totals Together
-        var totalLateFee = calcLateBooks + calcLateDvds
+            //Display Total Late Fee in Dollars on page
+            $("#totalOutput").text(totalLateFee.toFixed(2))
 
-        //Display Total Late Fee in Dollars on page
-        $("#totalOutput").text(totalLateFee.toFixed(2))
-
-        $(".display").show()
-    }
+            $(".display").show()
+        }
 
     }
 )
